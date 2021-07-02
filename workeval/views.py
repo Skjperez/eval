e20a011136f64021cab1f0c63d6387665a5d5292
Shonnetta Johnson-Perez <Skjperez@Shonnettas-MacBook-Pro.local>
2021-07-01 21:24:36 -0400

    created forms.py under workeval app

--- workeval/views.py
+++ workeval/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
 from .models import EvaluationTemplate
+from .forms import EvaluationTemplateForm
 
 # Create your views here.
 
 
 def CreateTemplate(request):
-    return HttpResponse('This is the beginning of this template')
-    #if request.method == 'POST':
-        #form = WorkEvaluationForm(request.POST)
-        #if form.is_valid():
+    if request.method == 'POST':
+        form = EvaluationTemplateForm(request.POST)
+        if form.is_valid():
+            item = EvaluationTemplate(
+             name=form.cleaned_data['name'],   
+            )
+            item.save()
+            return redirect('/workeval/create/')
+    else:
+        form = EvaluationTemplateForm()
+        context = {
+            'form' : form
+    }
+        return render(request, 'workeval/createtemplate.html', context)
 
 
 
